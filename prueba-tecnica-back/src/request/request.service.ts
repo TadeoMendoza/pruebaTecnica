e0bf3591c25f89039a3096b82e26760ef16b6959
TadeoMendoza <tadeomendoza905@gmail.com>
2026-09-18 15:06:01 -0300

    feat(request.service): Integracion de prisma para crear un post mediante api

--- prueba-tecnica-back/src/request/request.service.ts
+++ prueba-tecnica-back/src/request/request.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRequestDto } from './create-request.dto.js';
-
+import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class RequestService {
+  constructor(private readonly prisma: PrismaService) {}
   getRequests(): string {
     return 'Obteniendo solicitudes';
   }
 
-  processRequest(data: CreateRequestDto) {
-    function calculateMonthlyPayment(amount: number, months: number): number {
-      const annualRate = 0.24;
-      const monthlyRate = annualRate / 12;
-      const monthlyPayment =
-        amount * (monthlyRate / (1 - Math.pow(1 + monthlyRate, -months)));
-      const parsedMonthlyPayment = +monthlyPayment.toFixed(2);
-      return parsedMonthlyPayment;
+  private calculateMonthlyPayment(amount: number, months: number): number {
+    const annualRate = 0.24;
+    const monthlyRate = annualRate / 12;
+    const monthlyPayment =
+      amount * (monthlyRate / (1 - Math.pow(1 + monthlyRate, -months)));
+    const parsedMonthlyPayment = +monthlyPayment.toFixed(2);
+    return parsedMonthlyPayment;
+  }
+
+  async processRequest(data: CreateRequestDto) {
+    try {
+      const newRequest = await this.prisma.request.create({
+        data: {
+          name: data.name,
+          lastname: data.lastname,
+          dni: data.dni,
+          email: data.email,
+          phone: data.phone,
+          amount: data.amount,
+          months: data.months,
+          monthlyPayment: this.calculateMonthlyPayment(
+            data.amount,
+            data.months,
+          ),
+        },
+      });
+      return {
+        message: 'Solicitud procesada correctamente',
+        request: newRequest,
+      };
+    } catch (error) {
+      throw new Error('Error al procesar la solicitud');
     }
-    return {
-      message: 'Solicitud procesada correctamente',
-      solicitante: `${data.name} ${data.lastname}`,
-      meses: data.months,
-      cuota_mensual: calculateMonthlyPayment(data.amount, data.months),
-    };
   }
 }
