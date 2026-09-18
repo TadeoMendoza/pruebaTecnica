3ac03b1e8ebd788f37e915c8ab6ab43ef3e2c81b
TadeoMendoza <tadeomendoza905@gmail.com>
2026-09-18 12:52:53 -0300

    feat(request.service): Implementacion del calculo para cuota mensual

--- prueba-tecnica-back/src/request/request.service.ts
+++ prueba-tecnica-back/src/request/request.service.ts
@@ -8,10 +8,18 @@ export class RequestService {
   }
 
   processRequest(data: CreateRequestDto) {
+    const annualRate = 0.24;
+    const monthlyRate = annualRate / 12;
+    const amount = data.amount;
+    const months = data.months;
+    const monthlyPayment =
+      amount * (monthlyRate / (1 - Math.pow(1 + monthlyRate, -months)));
+    const parsedMonthlyPayment = +monthlyPayment.toFixed(2);
     return {
       message: 'Solicitud procesada correctamente',
       solicitante: `${data.name} ${data.lastname}`,
       meses: data.months,
+      cuota_mensual: parsedMonthlyPayment,
     };
   }
 }
