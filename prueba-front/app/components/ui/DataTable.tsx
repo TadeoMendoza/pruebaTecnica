a77b4127948e5ee18ff8d1e71c6b5f9d22bbe06b
TadeoMendoza <tadeomendoza905@gmail.com>
2026-09-19 15:33:02 -0300

    feat(getRequests): Implementacion de filtros basicos para el get, paginacion, status o reload

--- prueba-front/app/components/ui/DataTable.tsx
+++ prueba-front/app/components/ui/DataTable.tsx
@@ -1,32 +1,87 @@
+'use client'
+
 import { apiClient } from "@/app/server/api"
+import { useState, useEffect, useCallback } from "react";
 
-export async function DataTable() {
-
-    const getData = async () => {
+export function DataTable() {
+    const [pageFilter, setPageFilter] = useState(1);
+    const [limitFilter, setLimitFilter] = useState(10);
+    const [statusFilter, setStatusFilter] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState<any>(null);
+    const statuses = ['PENDING', 'APPROVED', 'REJECTED'];
+    const getData = useCallback(async () => {
+        setLoading(true);
         try {
-            const res = await apiClient('/solicitudes', {
+            const queryParams = new URLSearchParams({
+                page: pageFilter.toString(),
+                limit: limitFilter.toString(),
+                ...(statusFilter && { status: statusFilter }),
+            });
+
+            const res = await apiClient(`/solicitudes?${queryParams.toString()}`, {
                 method: 'GET',
                 headers: { 'Content-Type': 'application/json' },
             });
-            console.log("🚀 ~ getData ~ res:", res)
-            return res;
+
+            setData(res);
         } catch (error) {
-            console.error("🚀 ~ getData ~ error:", error)
-            return null;
+            console.error("🚀 ~ getData ~ error:", error);
+        } finally {
+            setLoading(false);
         }
-    }
-    const data = await getData();
-    const pages = data?.pages;
-    console.log("🚀 ~ DataTable ~ pages:", pages)
-    const results = data?.results;
-    console.log("🚀 ~ DataTable ~ results:", results)
-    const total = data?.total;
-    console.log("🚀 ~ DataTable ~ total:", total)
+    }, [pageFilter, limitFilter, statusFilter]);
+
+    useEffect(() => {
+        getData();
+    }, [getData]);
+
+    const results = data?.results || [];
+    const totalPages = data?.pages || 1;
     return (
         <div className="w-full max-w-7xl mx-auto p-10 bg-white/40 backdrop-blur-xs border border-white/20 rounded-2xl shadow-xl overflow-x-auto">
-            <h2 className="text-xl font-bold mb-4">Solicitudes</h2>
+            <div className="flex justify-between items-center">
+                <h2 className="text-xl font-bold">Solicitudes</h2>
+                <div className="flex gap-5 ">
+                    <button onClick={() => getData()} className="cursor-pointer transition-transform duration-300 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-reload">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M19.933 13.041a8 8 0 1 1 -9.925 -8.788c3.899 -1 7.935 1.007 9.425 4.747" />
+                            <path d="M20 4v5h-5" />
+                        </svg>
+                    </button>
+                    <select
+
+                        value={statusFilter}
+                        onChange={(e) => { setStatusFilter(e.target.value); setPageFilter(1) }}
+                        className=" p-2 mb-4 text-black rounded bg-white"
+                    >
+                        <option value="">Todos</option>
+                        {statuses.map((status) => (
+                            <option key={status} value={status}>
+                                {status}
+                            </option>
+                        ))}
+                    </select>
+                    <div>
+                        <button onClick={() => setPageFilter(pageFilter - 1)} disabled={pageFilter === 1} className="cursor-pointer transition-transform duration-300 hover:scale-110">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 6l-6 6l6 6" /></svg>
+                        </button>
+                        <span>{pageFilter}</span>
+                        <button onClick={() => setPageFilter(pageFilter + 1)} disabled={pageFilter === (totalPages || 1)} className="cursor-pointer transition-transform duration-300 hover:scale-110">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 18l6-6l-6-6" /></svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
             <table className="w-full">
-                <caption>Lista de usuarios</caption>
                 <thead>
                     <tr>
                         <th>Nombre</th>
@@ -57,7 +112,7 @@ export async function DataTable() {
                                 <td>{item.status}</td>
                                 <td>{new Date(item.createdAt).toLocaleString()}</td>
                                 <td>{new Date(item.updatedAt).toLocaleString()}</td>
-                            </tr> 
+                            </tr>
                         ))
                     ) : (
                         <tr>
@@ -68,6 +123,6 @@ export async function DataTable() {
                     )}
                 </tbody>
             </table>
-        </div>
+        </div >
     )
 }
--- prueba-front/app/page.tsx
+++ prueba-front/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
-import {Form} from "./components/ui/Form"
+import { Form } from "./components/ui/Form"
 import { DataTable } from "./components/ui/DataTable";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-10">
       <Form />
       <DataTable />
     </div>
   );
 }
- 
